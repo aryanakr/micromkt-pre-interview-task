f84fadbc46e51606001dc2ee849869ac182f9b2b
aryanakr <aryan.akr@yahoo.com>
2022-02-07 01:41:01 +0000

    cleanup gradle

--- app/src/androidTest/java/com/aryanakbarpour/micromktinterviewtest/TaskInstrumentedTest.kt
+++ app/src/androidTest/java/com/aryanakbarpour/micromktinterviewtest/TaskInstrumentedTest.kt
@@ -5,15 +5,12 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.aryanakbarpour.micromktinterviewtest.ui.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.hamcrest.Matchers
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import kotlin.concurrent.thread
 
 
 @HiltAndroidTest
